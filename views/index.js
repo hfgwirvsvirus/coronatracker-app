3bd91fc61b1e8478fbc80dba198a9e651f5542bd
Tobias Schmücker <schmuecker.tobias@gmail.com>
2020-03-22 19:01:51 +0100

    Restructure

--- views/index.js
+++ views/index.js
@@ -4,4 +4,4 @@ export { default as Intro } from './Onboarding/Intro';
 export { default as LocationPermission } from './Onboarding/Location';
 export { default as NotificationsPermission } from './Onboarding/Notifications';
 export { default as Login } from './Onboarding/Login';
-export { default as Profile } from './Profile';
+export { default as Profile } from './Profile/Profile';
